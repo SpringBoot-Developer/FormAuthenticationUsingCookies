fc3fe8527aefa80b699c6af822971fd8992be4c4
ROHAN8774 <rohan10bhakte0708@gmail.com>
2024-01-30 10:48:39 +0530

    Add project files.

---
+++ FormAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using FormAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormAuthentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                if(model.UserName == "admin" && model.Password == "123")
+                {
+                    Console.WriteLine("---");
+                    FormsAuthentication.SetAuthCookie(model.UserName , true);
+                    return RedirectToAction("Dashboard");
+                }
+            }
+
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.RemoveAll();
+            return View("Login");
+        }
+    }
+}
